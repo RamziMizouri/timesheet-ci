5038ba11f5dc05966d595e7cf3efa8d955c5e1f5
RamziMizouri <ramzi.mizouri@esprit.tn>
2021-07-30 15:33:05 +0100

    second commit

--- src/main/java/tn/esprit/spring/entities/Mission.java
+++ src/main/java/tn/esprit/spring/entities/Mission.java
@@ -14,7 +14,7 @@ import javax.persistence.OneToMany;
 public class Mission implements Serializable {
 
 	private static final long serialVersionUID = -5369734855993305723L;
-
+    // test commit 
 	// Auto Increment 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
